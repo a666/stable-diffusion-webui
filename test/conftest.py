453dc243d4bc351f14bd7e6aa1a3b437f4526cbe
a666 <19142162+a666@users.noreply.github.com>
2023-08-25 15:39:45 -0600

    Use pathlib

--- test/conftest.py
+++ test/conftest.py
@@ -1,14 +1,13 @@
-import os
+import base64
+from pathlib import Path
 
 import pytest
-import base64
+
+test_files_path = Path(__file__).parent / "test_files"
 
 
-test_files_path = os.path.dirname(__file__) + "/test_files"
-
-
-def file_to_base64(filename):
-    with open(filename, "rb") as file:
+def file_to_base64(filename: Path) -> str:
+    with Path(filename).open("rb") as file:
         data = file.read()
 
     base64_str = str(base64.b64encode(data), "utf-8")
@@ -17,9 +16,9 @@ def file_to_base64(filename):
 
 @pytest.fixture(scope="session")  # session so we don't read this over and over
 def img2img_basic_image_base64() -> str:
-    return file_to_base64(os.path.join(test_files_path, "img2img_basic.png"))
+    return file_to_base64(Path(test_files_path) / "img2img_basic.png")
 
 
 @pytest.fixture(scope="session")  # session so we don't read this over and over
 def mask_basic_image_base64() -> str:
-    return file_to_base64(os.path.join(test_files_path, "mask_basic.png"))
+    return file_to_base64(Path(test_files_path) / "mask_basic.png")
