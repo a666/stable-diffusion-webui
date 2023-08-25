405d38768b60f88c25e38fef3f0f0689763a4382
a666 <19142162+a666@users.noreply.github.com>
2023-08-25 16:46:24 -0600

    Use pathlib

--- modules/ui_tempdir.py
+++ modules/ui_tempdir.py
@@ -4,48 +4,49 @@ from collections import namedtuple
 from pathlib import Path
 
 import gradio.components
-
 from PIL import PngImagePlugin
 
 from modules import shared
 
-
-Savedfile = namedtuple("Savedfile", ["name"])
+Savedfile = namedtuple("Savedfile", ["name"])  # TODO: replace by typed NamedTuple
 
 
 def register_tmp_file(gradio, filename):
-    if hasattr(gradio, 'temp_file_sets'):  # gradio 3.15
-        gradio.temp_file_sets[0] = gradio.temp_file_sets[0] | {os.path.abspath(filename)}
+    if hasattr(gradio, "temp_file_sets"):  # gradio 3.15
+        gradio.temp_file_sets[0] = gradio.temp_file_sets[0] | {Path(filename).resolve()}
 
-    if hasattr(gradio, 'temp_dirs'):  # gradio 3.9
-        gradio.temp_dirs = gradio.temp_dirs | {os.path.abspath(os.path.dirname(filename))}
+    if hasattr(gradio, "temp_dirs"):  # gradio 3.9
+        gradio.temp_dirs = gradio.temp_dirs | {Path(filename).parent.resolve()}
 
 
 def check_tmp_file(gradio, filename):
-    if hasattr(gradio, 'temp_file_sets'):
+    if hasattr(gradio, "temp_file_sets"):
         return any(filename in fileset for fileset in gradio.temp_file_sets)
 
-    if hasattr(gradio, 'temp_dirs'):
-        return any(Path(temp_dir).resolve() in Path(filename).resolve().parents for temp_dir in gradio.temp_dirs)
+    if hasattr(gradio, "temp_dirs"):
+        return any(
+            Path(temp_dir).resolve() in Path(filename).resolve().parents
+            for temp_dir in gradio.temp_dirs
+        )
 
     return False
 
 
 def save_pil_to_file(self, pil_image, dir=None, format="png"):
-    already_saved_as = getattr(pil_image, 'already_saved_as', None)
-    if already_saved_as and os.path.isfile(already_saved_as):
+    already_saved_as = getattr(pil_image, "already_saved_as", None)
+    if already_saved_as and Path(already_saved_as).is_file():
         register_tmp_file(shared.demo, already_saved_as)
         filename = already_saved_as
 
         if not shared.opts.save_images_add_number:
-            filename += f'?{os.path.getmtime(already_saved_as)}'
+            filename += f"?{Path(already_saved_as).stat().st_mtime}"
 
         return filename
 
     if shared.opts.temp_dir != "":
         dir = shared.opts.temp_dir
     else:
-        os.makedirs(dir, exist_ok=True)
+        Path(dir).mkdir(parents=True, exist_ok=True)
 
     use_metadata = False
     metadata = PngImagePlugin.PngInfo()
@@ -68,21 +69,21 @@ def on_tmpdir_changed():
     if shared.opts.temp_dir == "" or shared.demo is None:
         return
 
-    os.makedirs(shared.opts.temp_dir, exist_ok=True)
+    Path(shared.opts.temp_dir).mkdir(parents=True, exist_ok=True)
 
-    register_tmp_file(shared.demo, os.path.join(shared.opts.temp_dir, "x"))
+    register_tmp_file(shared.demo, Path(shared.opts.temp_dir, "x"))
 
 
 def cleanup_tmpdr():
     temp_dir = shared.opts.temp_dir
-    if temp_dir == "" or not os.path.isdir(temp_dir):
+    if temp_dir == "" or not Path(temp_dir).is_dir():
         return
 
     for root, _, files in os.walk(temp_dir, topdown=False):
         for name in files:
-            _, extension = os.path.splitext(name)
+            extension = Path(name).suffix
             if extension != ".png":
                 continue
 
-            filename = os.path.join(root, name)
-            os.remove(filename)
+            filename = Path(root, name)
+            Path(filename).unlink()
